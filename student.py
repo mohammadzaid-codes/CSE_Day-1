4313a81b53dc2e291aa735eb07581c41f60db3a4
n76wpvbqxz-spec <n76wpvbqxz@privaterelay.appleid.com>
2026-01-14 01:25:06 +0530

    Day 1 as a sudent info project typed manualy

---
+++ student.py
@@ -0,0 +1,9 @@
+print("welcome to student info prgram")
+name = input("Enter student name")
+math = int(input("Enter Math Marks"))
+python_marks = int(input("Enter Python Marks"))
+total = math  + python_marks
+average = total/2
+print("Student Name", name)
+print("Total Marks", total)
+print("Average Marks", average)
