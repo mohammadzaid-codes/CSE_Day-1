30dc463266fa1b73c4ebbdf0dedbc895d49ffabe
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-15 11:08:36 +0530

    Day 2: if else and input programs

---
+++ day2.py
@@ -0,0 +1,20 @@
+name = input("Enter your Name: ")
+age = int(input("Enter your Age: "))
+
+if age >= 18:
+    print(name , "you are eligible to vote")
+else:
+    print(name ," you aree not eligible to vote")
+
+
+
+Student_name = input("Enter Student Name :")
+marks = int(input("Enter Student Marks"))
+if marks >= 40:
+    print(Student_name, "PASSED")
+else:
+    print(Student_name, "FAILED")
+
+
+
+                      
