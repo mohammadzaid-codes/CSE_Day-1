027251e6f9d7e2c7c2db9984c98c5f8fd2a39dd0
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-16 11:56:26 +0530

    Day 3: added Grade D Condition and Change in username

--- day3.py
+++ day3.py
@@ -5,6 +5,8 @@ elif marks >= 75:
     print("Grade B ")
 elif marks >=60:
     print("Grade C ")
+elif marks >=40:
+    print("Grade D")
 else:
     print("FAIL")
 
@@ -13,7 +15,7 @@ else:
 
     username = input("enter Username:")
     password = input("enter password:")
-    if username == "admin" and password == "1234":
+    if username == "zaid" and password == "1234":
         print("Login Sucesfully")
     else:
         print("Invalid Ceredentials")
