3587922ac9947dce94995a82d201af72f3eed0f2
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-17 17:23:59 +0530

    Day 4: if-else practice completed

---
+++ day4.py
@@ -0,0 +1,40 @@
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
+
+
+
+
+
+num = int(input("Enter a number:"))
+if num > 0:
+    print("positive number")
+elif num< 0:
+    print("negative number")
+else:
+    print("zero")
+
+
+
+
+
+    a = int(input("Enter first number:"))
+    b = int(input("Enter second number:"))
+    sum = a+b 
+    print ("the sum of a nad b is",sum)
+
+
+
+
+    a = int(input("Enter first Number:"))
+    b = int(input("Enter second number:"))
+    if a>b :
+        print("First number is greater")
+    elif b>a:
+        print("Second number is greater")
+    else:
+        print("Both are equal")
+        
+        
