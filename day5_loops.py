d7a94076444aa69e5a269e6cb1dcb1efec7457df
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-18 14:38:24 +0530

    Day 5:  loops practice

---
+++ day5_loops.py
@@ -0,0 +1,39 @@
+for i in range(1,11):
+    print(i)
+
+
+
+i = 1
+while i <= 5:
+    print(i)
+    i+=1
+
+
+
+
+
+    for i in range(2,21,2):
+        print(i)
+
+    
+
+    total_sum = sum(range(1,11))
+    print ("the sum of first 10 number is", total_sum)
+
+
+
+    number = int(input("Enter a Number:"))
+    if number % 2 == 0:
+        print ("number is even")
+    else:
+        print ("number is odd")
+
+
+
+       
+
+    num = 5
+    for i in range(1,11):
+        print(num*i)
+
+                 
