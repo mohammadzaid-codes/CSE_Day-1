5cacee0486d2cfb22711f83c556d0dbfbed99631
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-19 18:59:26 +0530

    Day 6 Mini-Project: Calculator using functions - Created add, subtract, multiply, divide functions - Used if/elif/else to select operation - Handles user input and prints results - File: calculator.py

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def add(a,b):
+    return a+b
+
+def substract(a, b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b == 0:
+        return "Cannot divided by zero"
+        return a/b
+    
+num1 = float(input("Enter first number"))
+num2 =  float(input("Enter second number"))
+
+op = input("Enter operation (+,-,*,/):")
+if op == "+":
+        print("Result", add(num1,num2))
+elif op == "-":
+        print("Result", substract(num1,num2))
+elif op == "*":
+        print("Result", multiply(num1,num2))
+elif op == "/":
+        print("Result", divide(num1,num2))
+else:
+        print("Invalid operation")
+              
+
