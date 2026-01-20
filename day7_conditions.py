15f88854169f164caec3cadc8bac77e910d4c4b8
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-20 11:40:51 +0530

    Day 7: completed conditions programms practice

---
+++ day7_conditions.py
@@ -0,0 +1,43 @@
+age = int(input("Enter your age"))
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+
+
+number = int(input("Enter a number"))
+if number % 2 ==0:
+        print("The number is Even")
+else:
+        print("The number is Odd")
+
+
+
+marks = int(input("Enter your marks"))
+if marks >= 40:
+      print("PASS")
+else:
+      print("FAIL")
+
+
+
+
+marks = int(input("Enter your marks"))
+if marks >= 90:
+      print("Grade A ")
+elif marks >= 75:
+      print("Grade B ")
+elif marks >= 60:
+      print("Grade C ")
+elif marks >= 40:
+      print("Grade D ")
+else:
+      print ("FAIL")
+
+
+
+
+
+      
+      
