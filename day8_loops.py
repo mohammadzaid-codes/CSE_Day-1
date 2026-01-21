fffbc6af65a409ac4ba641317480ac1d8c8de3f6
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-21 12:51:51 +0530

    Day 8 : Completed for and while loop practice

---
+++ day8_loops.py
@@ -0,0 +1,26 @@
+for i in range (1,11):
+    print(i)
+
+
+
+
+    num = int(input("Enter anumber for table"))
+
+    for i in range(1,11):
+      print( num*i)
+
+
+
+
+total_sum = 0
+for i in range (1,11):
+ total_sum = total_sum + i
+print ("the sum is", total_sum)
+
+
+
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i+ 1
