82146fc0fa136a1edb02c9d6604ec84819bcd4f8
mohammadzaid-codes <zaif38849@gmail.com>
2026-01-29 21:31:40 +0530

    Day 16 : Practiced loops by thinking, not copying

---
+++ day16_loops_thinking.py
@@ -0,0 +1,22 @@
+for i in range(5):
+         print(i)
+
+for i in range(1,6):
+         print(i)
+
+for i in range(2,21,2):
+           print("Even number is" , i)
+
+ 
+total_sum = sum(range(1,11))
+print("sum is ",total_sum)
+
+number = int(input("Enter a number"))
+for i in range(1,11):
+            print(i*number)
+                
+
+i = "10,9,8,7,6,5,4,3,2,1"
+print(i)
+
+    
